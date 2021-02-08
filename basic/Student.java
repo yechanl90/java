13b91e2530596c219c40ad9e2dc3525d6eec8a73
yechan <yechanl90@gmail.com>
2021-02-08 21:46:21 +0900

    modify Student Class

--- basic/Student.java
+++ basic/Student.java
@@ -15,4 +15,25 @@ public class Student {
 	void leaveEarly() { 
 		leaveEarlySum  = leaveEarlySum + 1;
 	}
+
+	public static void main(String[] args) {
+		Student hong = null;
+		Student im = null;
+		
+		hong = new Student();
+		hong.name = "홍길동";
+		hong.mobile = "010-1111-2222";
+	
+		im = new Student();
+		im.name = "임꺽정";
+		im.mobile = "010-3333-4444";
+	
+		hong.absent();
+		hong.absent();
+
+		im.absent();
+		
+		System.out.println(hong.absentSum);
+		System.out.println(im.absentSum);
+	}
 }
