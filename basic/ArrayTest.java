0e99c33c7e1bf409648ce6dbb66dac8bd2455daa
yechan <yechanl90@gmail.com>
2021-02-08 22:07:24 +0900

    create ArrayTest class

---
+++ basic/ArrayTest.java
@@ -0,0 +1,7 @@
+public class ArrayTest {
+	public static void main(String[] args) {
+		System.out.println(args[0] + "년");
+		System.out.println(args[1] + "월");
+		System.out.println(args[2] + "일");
+	}
+}
