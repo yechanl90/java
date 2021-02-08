c0bde19186da8af36747fb19a3de54cc5b5f6d24
yechan <yechanl90@gmail.com>
2021-02-08 21:47:37 +0900

    create StandardOutput Class

---
+++ basic/StandardOutput.java
@@ -0,0 +1,16 @@
+public class StandardOutput {
+
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println('A');
+		char[] x = {'A', 'B', 'C'};
+		System.out.println(x);
+		System.out.println(99.9);
+		System.out.println();
+		System.out.println(99.9F);
+		System.out.println(100);
+		System.out.println(40000000L);
+		System.out.println(System.out);
+		System.out.println("표준출력메소드");
+	}
+}
